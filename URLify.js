90a4b68668c4790f59cfe56c4bd1c9621d78983d
j-tirelli <john.tirelli@gmail.com>
2020-11-29 01:58:51 -0500

    solution for URLify

--- URLify.js
+++ URLify.js
@@ -1,7 +1,9 @@
 // Implement an algorithm to convert a string with spaces between words into a URI with %20 in place of spaces.
 
 const URLify = (string1) => {
-
+  let trimmedString = string1.trim();
+  let splitString = trimmedString.split(' ');
+  return splitString.join('%20')
 };
 
 module.exports = URLify;
